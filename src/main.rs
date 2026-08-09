e761e2722fd405d6d2572ea2b97f40110520e350
Fiqih Fathor Rachim <fiqih.fathor.rachim@gmail.com>
2026-08-09 15:06:57 +0700

    fix: typo unwarp to unwrap

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("Server running on http://{}", addr);
 
-    let listener = tokio::net::TcpListener::bind(addr).await.unwarp();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
